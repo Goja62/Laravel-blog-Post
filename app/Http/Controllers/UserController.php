3cd7cf78696c8a2fac6e84b46c76e76517496713
Goja <gortnar@beograd.com>
2026-01-26 10:58:20 +0100

    Login, logout, impruve registar form, delete ExampleControler and about function in web.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,10 +5,40 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/')->with('success', 'You are now ogged out.');
+    }
+
+    public function showCorrectHomepage()
+    {
+        if (Auth::check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (Auth::attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You have succesfuly logged in.');
+        } else {
+            return redirect('/')->with('failure', 'Invalid login');
+        }
+    }
+
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
@@ -17,7 +47,9 @@ class UserController extends Controller
             'password' => ['required', 'min:5', 'confirmed'],
 
         ]);
-        User::create($incomingFields);
-        return 'Registred!';
+
+        $user =  User::create($incomingFields);
+        Auth::login($user);
+        return redirect('/')->with('success', 'Thank you for creating account');
     }
 }
