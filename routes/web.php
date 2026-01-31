92ece961d70199ddb7bb1b88f942f0957d698103
Your NameGoja <you@example.comgortnar@beograd.com>
2026-01-31 16:42:16 +0100

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,17 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // User related routes
-Route::get('/', [UserController::class, 'showCorrectHomepage']);
+Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
 
-Route::post('/register', [UserController::class, 'register']);
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');
 
-Route::post('login', [UserController::class, 'login']);
+Route::post('login', [UserController::class, 'login'])->middleware('guest');
 
-Route::post('logout', [UserController::class, 'logout']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
 
 // Blog post related routes
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
 
-Route::post('/create-post', [PostController::class, 'storeNewPost']);
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
 
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
