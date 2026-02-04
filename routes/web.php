a12818820ad039237406751abad7479a73d0ffe1
Your NameGoja <you@example.comgortnar@beograd.com>
2026-02-04 15:39:47 +0100

    Admin Only Pages (What is a Gate?)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Gate as FacadesGate;
 use Illuminate\Support\Facades\Route;
 
 // User related routes
@@ -28,3 +29,15 @@ Route::put('/post/{post}', [PostController::class, 'actuallyUpdate'])->middlewar
 
 // Profile related routes
 Route::get('/profile/{user:username}', [UserController::class, 'profile']);
+
+
+
+Route::get('/admin-only', function () {
+    // if (FacadesGate::allows('visitAdminPages')) {
+    //     return 'Only admin can see this page';
+    // }
+
+    // return 'You cannot view this page';
+
+    return 'Only admin can see this page';
+})->middleware('can:visitAdminPages');
